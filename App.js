c8f80e1d262bb8b9d84b3d4ffcff48536449d0fc
Parker Cooper <parkr.coopr@gmail.com>
2019-10-27 18:27:04 -0400

    connected to api

--- App.js
+++ App.js
@@ -1,50 +1,42 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Query } from 'react-apollo'
 
-const sampleQuery = gql`
- query {
-  links {
-    postedBy {
-      name
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        postedBy {
+          name
+        }
+        createdAt
+        url
+        id
+        description
+        votes {
+          user {
+            name
+          }
+        }
+      }
+      count
     }
   }
-}
-`;
-
-// const DogComponent = graphql(dogQuery)(props => {
-//   const { links } = props.data;
-//   console.log('lol',links)
-//   return (
-//     <View>
-//       {links.map((link)=>{
-//         return <Text></Text>
-//       })}
-//     </View>
-//   )
-//   // if (error) {
-//   //   return <Text>{error}</Text>;
-//   // }
-//   // if (dogs) {
-//   //   return <Text>{dogs[0].name}</Text>;
-//   // }
-
-//   // return <Text>Loading...</Text>;
-// });
-
+`
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://eu1.prisma.sh/parker-cooper-88b08c/tutorial/tutorialdev',
+    uri: 'http://192.168.86.32:4000',
     headers: {
       authorization: ''
     }
   }),
   cache: new InMemoryCache()
 });
-
 
 
 export default function App() {
@@ -54,7 +46,28 @@ export default function App() {
     <ApolloProvider client={client}>
       <View style={styles.container}>
         <Text>{outputText}</Text>
-        {/* <DogComponent /> */}
+        <Query query={FEED_QUERY}>
+          {({ loading, error, data }) => {
+            console.log('lol',data)
+            if (loading) return <Text>Fetching</Text>
+            if (error) return <Text>oops</Text>
+
+            const linksToRender = data.feed.links 
+
+            return (
+                <ScrollView>
+                  {linksToRender.map((link, index) => (
+                    <View key={link.id} style={styles.post_container}>
+                      <Text>{link.postedBy.name}</Text>
+                      <Text>{link.url}</Text>
+                      <Text>{link.description}</Text>
+                    </View>
+                  ))}
+                </ScrollView>
+              )
+          }}
+        </Query>
+
 
         <Button title="who" onPress={()=> setOutputText('man o man')}></Button>
       </View>
@@ -65,9 +78,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gray',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    color: 'white'
+  },
+  post_container: {
+    backgroundColor: 'red',
+    width: 200,
+    borderRadius: 100,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    margin: 5,
+    padding: 10
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  activeTitle: {
+    color: 'red',
   },
 });
